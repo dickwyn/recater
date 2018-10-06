4d15f970f79f36b58a69b2c442e519710eebcea9
Dickwyn <dickwyn@outlook.com>
2018-10-06 15:20:07 -0700

    update homescreen and readme

--- README.md
+++ README.md
@@ -1,6 +1,31 @@
 # Where is Jimmy
 Created at Hacks for Humanity 2018 at Arizona State University (Oct 7 - 8, 2018).
 
+## Running the Project 
+
+**Installation**
+
+Clone or fork this repository to your local machine:
+```
+$ git clone https://github.com/dickwyn/table6.git
+```
+Install the depencies with the following commands:
+```
+$ npm run client-install
+```
+
+**Prerequisites**
+
+These services must be installed:
+- [Git](https://git-scm.com/downloads)
+- [Node.js](https://nodejs.org/en/download/)
+- [NPM](https://www.npmjs.com/get-npm)
+
+**Development Mode**
+
+1. `$ npm start`
+2. Browsersync can be accessed through `localhost:3000`
+
 ## License
 
 The contents of this repository are covered under the [MIT License](https://github.com/dickwyn/table6/blob/master/LICENSE).
--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Jumbotron, Button, Col, Row, Container } from 'reactstrap';
+import Header from '../src/Components/Header';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,20 +8,42 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <Header />
+        <Container>
+          <Row>
+            <Col>.col</Col>
+          </Row>
+          <Row>
+            <Col>.col</Col>
+            <Col>.col</Col>
+            <Col>.col</Col>
+            <Col>.col</Col>
+          </Row>
+          <Row>
+            <Col xs="3">.col-3</Col>
+            <Col xs="auto">.col-auto - variable width content</Col>
+            <Col xs="3">.col-3</Col>
+          </Row>
+          <Row>
+            <Col xs="6">.col-6</Col>
+            <Col xs="6">.col-6</Col>
+          </Row>
+          <Row>
+            <Col xs="6" sm="4">.col-6 .col-sm-4</Col>
+            <Col xs="6" sm="4">.col-6 .col-sm-4</Col>
+            <Col sm="4">.col-sm-4</Col>
+          </Row>
+          <Row>
+            <Col sm={{ size: 6, order: 2, offset: 1 }}>.col-sm-6 .col-sm-order-2 .col-sm-offset-2</Col>
+          </Row>
+          <Row>
+            <Col sm="12" md={{ size: 8, offset: 2 }}>.col-sm-12 .col-md-6 .col-md-offset-3</Col>
+          </Row>
+          <Row>
+            <Col sm={{ size: 'auto', offset: 1 }}>.col-sm .col-sm-offset-1</Col>
+            <Col sm={{ size: 'auto', offset: 1 }}>.col-sm .col-sm-offset-1</Col>
+          </Row>
+        </Container>
       </div>
     );
   }
