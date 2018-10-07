5d57aa52d8640de8d07852a82859bfc3f1785ed6
Dickwyn <dickwyn@outlook.com>
2018-10-06 22:15:28 -0700

    update food selection page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Header from '../src/Components/Header';
-import Indicator from '../src/Components/Indicator';
+import Footer from '../src/Components/Footer';
 import logo from './logo.svg';
 import './App.css';
 
@@ -9,10 +9,27 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
-        <Indicator />
-        <footer>
-          Not for display
-        </footer>
+        <div className="container">
+          <div className="title">
+              <h2>Reducing food waste for food equality</h2>
+          </div>
+          <div class="subtitle">
+            <p>
+              Many catering events have food waste, which gets thrown away, creating food waste. In fact, the United States is the leading producer of food waste. By donating your leftover food to a non-profit organization in need of food, you will be serving your community by feeding those who are hungry.
+            </p>
+          </div>
+          <div className="card-l">
+            <h3>Hacks for Humanity</h3>
+            <h6>🥘 Vegetarian and Non-vegetarian</h6>
+            <h6><center>📍 5 miles away</center></h6>
+          </div>
+          <div className="card-r">
+            <h3>International Night</h3>
+            <h6>🥘 Meat</h6>
+            <h6><center>📍 2 miles away</center></h6>
+          </div>
+        </div>
+        <Footer />
       </div>
     );
   }
---
+++ client/src/Components/Footer/index.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './style.css';
+
+export default class Footer extends Component {
+    render() {
+        return (
+        <div>
+            <div className="container-footer">
+                Project by Team Table 6
+                <br/>
+                Hacks for Humanities 2018
+            </div>
+        </div>
+    );
+  }
+}
