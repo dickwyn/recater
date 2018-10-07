b49db6cb19d6dd89e4e105bed0c4737947da4e0d
Dickwyn <dickwyn@outlook.com>
2018-10-06 17:59:36 -0700

    working color function

--- client/src/Components/Indicator/index.jsx
+++ client/src/Components/Indicator/index.jsx
@@ -3,44 +3,54 @@ import { Progress } from 'reactstrap';
 import './style.css';
 
 export default class Indicator extends Component {
-  render() {
-    return (
-      <div>
-        <div className="container">
-            <div className="time">
-                <h5>Time</h5>
-                <Progress color="warning" value={75}>75% done</Progress>
-            </div>
-            <div className="temperature">
-                <h5>Temperature</h5>
-                <div className="card">
-                    70
-                </div>
-            </div>
-            <div className="heartrate">
-                <h5>Heart Rate</h5>
-                <div className="card">
-                    150bpm
-                </div>
-            </div>
-            <div className="humidity">
-                <h5>Humidity</h5>
-                <div className="card">
-                    200ml/g
-                </div>
-            </div>
-            <div className="sound">
-                <h5>Sound</h5>
+    temperature = (temp) => {
+        if (temp > 0){
+            return "red"
+        }
+        else if(temp > 80) {
+            return "blue"
+        }
+    }
 
-            </div>
-            <div className="acceleration">
-                <h5>Acceleration</h5>
-                <div className="card">
-                    82
-                </div>              
+    render() {
+        return (
+        <div>
+            <div className="container">
+                <div className="time">
+                    <h5>Time</h5>
+                    <Progress color="warning" value={75}>75% done</Progress>
+                </div>
+                <div className="temperature">
+                    <h5>Temperature</h5>
+                    <div className="card" style={{background: this.temperature(75)}}>
+                        70
+                        {this.temperature(80)}
+                    </div>
+                </div>
+                <div className="heartrate">
+                    <h5>Heart Rate</h5>
+                    <div className="card">
+                        150bpm
+                    </div>
+                </div>
+                <div className="humidity">
+                    <h5>Humidity</h5>
+                    <div className="card">
+                        200ml/g
+                    </div>
+                </div>
+                <div className="sound">
+                    <h5>Sound</h5>
+
+                </div>
+                <div className="acceleration">
+                    <h5>Acceleration</h5>
+                    <div className="card">
+                        82
+                    </div>              
+                </div>
             </div>
         </div>
-      </div>
     );
   }
 }
